58b16ce5574b422fd86ca9f02315cfadd94db502
rodrigoturell <rodrigoturell@gmail.com>
2023-07-08 22:59:39 -0300

    add product name and total value

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ function calcEdad(añoActual, añoNacimiento) {
     return añoActual - añoNacimiento
 }
 
+// Preguntar nombre del producto
+function preguntarProducto() {
+    return prompt('¿Qué quiere comprar?');
+}
+
 // Preguntar costo del producto
 function preguntarCosto() {
-    return Number(prompt('¿Cuánto sale lo que querés comprar? \nIngresá solo el número.'))
+    return Number(prompt('¿Cuál es el costo?'))
 }
 
 // Calcular el Iva
@@ -13,14 +18,13 @@ function calcIva(precioProducto, iva) {
     return precioProducto * iva;
 }
 
-
 let añoNacimiento = Number(prompt('Hola! \n¿En que año naciste?'))
 let fecha = new Date();
 let añoActual = fecha.getFullYear();
 let edadUsuario = calcEdad(añoActual, añoNacimiento)
 let iva = 0.21;
+let total= 0;
 let exit;
-
 
 // Condicional para acceder al sitio u obtener descuentos dependiendo de la edad
 if (edadUsuario < 18) {
@@ -31,18 +35,21 @@ if (edadUsuario < 18) {
     alert('Bienvenido al sitio');
 
     do {
+        let nombreProducto = preguntarProducto();
         let precioProducto = preguntarCosto();
         let precioFinal = calcIva(precioProducto, iva) + precioProducto;
-        alert('El precio es de: ' + precioProducto + '\nEl costo total más iva es de ' + '$' + precioFinal + '\nGracias por tu compra! :)')
-        exit = prompt('¿Queres seguir comprando? \nDigita cualquier tecla para seguir. \nDigita ESC para salir.').toUpperCase()
+        total = total + precioFinal;
+        alert('El precio de ' + nombreProducto + ' es de ' + '$' + precioProducto + '\nEl costo + iva es de ' + '$' + precioFinal + '\n\n\n El precio total de su compra es de: ' + '$' + total)
+        exit = prompt('¿Desea seguir comprando? \nDigite cualquier tecla para seguir. \nDigite ESC para salir.').toUpperCase()
     } while (exit != 'ESC')
 
 } else if (edadUsuario < 110) {
 
     do {
         let precioProducto = preguntarCosto();
-        alert('Felicidades! Por tener 60 o más no pagás IVA! \nEl precio total es de:  ' + '$' + precioProducto)
-        exit = prompt('¿Querés seguir comprando? \nDigita cualquier tecla para seguir. \nDigita ESC para salir.').toUpperCase()
+        total = total + precioProducto;
+        alert('¡Felicidades, por tener 60 años o más no paga Iva! \nEl precio de su producto es de:  ' + '$' + precioProducto + '\n\nEl precio total de su compra es de ' + '$' + total)
+        exit = prompt('¿Desea seguir comprando? \nDigite cualquier tecla para seguir. \nDigite ESC para salir.').toUpperCase()
     } while (exit != 'ESC')
 
 } else {
